0a9920eb12743e6a71227c1b0e49966809eb65db
Nikolay123d <urciknikolaj642@gmail.com>
2025-08-27 17:06:44 +0200

    Add files via upload
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+window.PF_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDw_bVibsVyZegH7OJyZ_yRjI3uLhroVBk",
+  authDomain: "praga-4baee.firebaseapp.com",
+  databaseURL: "https://praga-4baee-default-rtdb.firebaseio.com",
+  projectId: "praga-4baee",
+  storageBucket: "praga-4baee.firebasestorage.app",
+  messagingSenderId: "336023952536",
+  appId: "1:336023952536:web:f7437feaa25b6eadcd04ed",
+  measurementId: "G-BGDJD6R12N"
+};
