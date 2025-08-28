1e31e8c8e7889de4b0dc7ebf131c5706c41338e6
Nikolay123d <urciknikolaj642@gmail.com>
2025-08-28 14:04:49 +0200

    Update config.js
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

--- firebase/config.js
+++ firebase/config.js
@@ -1,5 +1,12 @@
-// Firebase (compat API)
-window.PF_FIREBASE_CONFIG = {
+ Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyDw_bVibsVyZegH7OJyZ_yRjI3uLhroVBk",
   authDomain: "praga-4baee.firebaseapp.com",
   databaseURL: "https://praga-4baee-default-rtdb.firebaseio.com",
@@ -9,3 +16,7 @@ window.PF_FIREBASE_CONFIG = {
   appId: "1:336023952536:web:f7437feaa25b6eadcd04ed",
   measurementId: "G-BGDJD6R12N"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
