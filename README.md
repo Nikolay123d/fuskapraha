5638f67a724fbadbc26d9a0b6513e9552d07c26a
Nikolay123d <urciknikolaj642@gmail.com>
2025-08-30 21:55:13 +0200

    Add files via upload
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# PRÁCE CZ CHAT v30
+
+Запусти через http/https. У консолі Firebase увімкни Auth (Email+Google), Storage, і додай `database.rules.json` у Realtime Database.
---
+++ config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDw_bVibsVyZegH7OJyZ_yRjI3uLhroVBk",
+  authDomain: "praga-4baee.firebaseapp.com",
+  databaseURL: "https://praga-4baee-default-rtdb.firebaseio.com",
+  projectId: "praga-4baee",
+  storageBucket: "praga-4baee.appspot.com",
+  messagingSenderId: "336023952536",
+  appId: "1:336023952536:web:f7437feaa25b6eadcd04ed",
+  measurementId: "G-BGDJD6R12N"
+};
+export const ADMIN_EMAIL = "urciknikolaj642@gmail.com";
+export const FREE_LIMITS = { text: 200, photo: 100 };
