e33b2888c859cd9053682da507964102166c4411
Nikolay123d <urciknikolaj642@gmail.com>
2025-09-05 23:25:10 +0200

    Delete admin.js
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

--- admin.js
+++
@@ -1,4 +0,0 @@
-// Admin helpers
-window.isAdminEmail = (email)=> email && email.toLowerCase() === (window.PF.ADMIN_EMAIL||'').toLowerCase();
-async function ensureAdminPath(){ try{ await fb.db.ref("settings/adminEmail").set(window.PF.ADMIN_EMAIL); }catch(e){} }
-ensureAdminPath();
