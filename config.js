07754212163265e8f23283ad4b631958ab014f7e
Nikolay123d <urciknikolaj642@gmail.com>
2025-09-07 09:31:35 +0200

    Add files via upload
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

---
+++ config.js
@@ -0,0 +1,12 @@
+// Using Firebase compat SDK via CDN. Do NOT use ESM imports here.
+window.PF_CONFIG = {
+  "apiKey": "AIzaSyDw_bVibsVyZegH7OJyZ_yRjI3uLhroVBk",
+  "authDomain": "praga-4baee.firebaseapp.com",
+  "databaseURL": "https://praga-4baee-default-rtdb.firebaseio.com",
+  "projectId": "praga-4baee",
+  "storageBucket": "praga-4baee.firebasestorage.app",
+  "messagingSenderId": "336023952536",
+  "appId": "1:336023952536:web:f7437feaa25b6eadcd04ed",
+  "measurementId": "G-BGDJD6R12N"
+};
+window.PF_ADMIN_EMAIL = "urciknikolaj642@gmail.com";
