a51b68465c1dd6e175c876ddb285a986fb4dc529
Nikolay123d <urciknikolaj642@gmail.com>
2025-10-20 15:42:35 +0200

    Add files via upload
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

---
+++ config.js
@@ -0,0 +1,20 @@
+// ==== Firebase config
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDw_bVibsVyZegH7OJyZ_yRjI3uLhroVBk",
+  authDomain: "praga-4baee.firebaseapp.com",
+  databaseURL: "https://praga-4baee-default-rtdb.firebaseio.com",
+  projectId: "praga-4baee",
+  storageBucket: "praga-4baee.appspot.com",
+  messagingSenderId: "336023952536",
+  appId: "1:336023952536:web:f7437feaa25b6eadcd04ed",
+  measurementId: "G-BGDJD6R12N"
+};
+
+// Admin email
+window.ADMIN_EMAIL = "urciknikolaj642@gmail.com";
+
+// Use Firebase Storage? If false — images are saved as dataURL into DB
+window.USE_STORAGE = false;
+
+// Default avatar
+window.DEFAULT_AVATAR = "./img/default-avatar.svg";
