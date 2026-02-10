f67dea4a4a8eca7fd0460766c7cfc1647433d80f
Nikolay123d <urciknikolaj642@gmail.com>
2026-02-10 21:56:53 +0100

    Delete assets directory
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

--- assets/js/firebase.init.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  firebase.initializeApp({
-    apiKey: "PASTE_YOUR_KEY",
-    authDomain: "PROJECT.firebaseapp.com",
-    databaseURL: "https://PROJECT.firebaseio.com",
-    projectId: "PROJECT"
-  });
-  console.log('[firebase.init] OK');
-})();
