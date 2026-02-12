caa4a4f17eab46a6734f10faaf7498ac3ca21e18
Nikolay123d <urciknikolaj642@gmail.com>
2026-02-12 10:46:31 +0100

    Delete assets directory
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

--- assets/js/modules/core/01_dom.js
+++
@@ -1,4 +0,0 @@
-export const q=(s,root=document)=>root.querySelector(s);
-export const qa=(s,root=document)=>Array.from(root.querySelectorAll(s));
-
-export function safeAppend(parent, child){ if(parent && child && parent.appendChild) parent.appendChild(child); }
