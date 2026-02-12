ed6effe3dbed44a89dd6a88500b05fcf7d4463e3
Nikolay123d <urciknikolaj642@gmail.com>
2026-02-12 03:00:22 +0100

    Delete assets directory
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

--- assets/js/modules/core/03_logger.js
+++
@@ -1,6 +0,0 @@
-
-import { ENV } from "./00_env.js";
-
-export function log(...a){ if(ENV.DEBUG) console.log("[mk]", ...a); }
-export function warn(...a){ console.warn("[mk]", ...a); }
-export function err(...a){ console.error("[mk]", ...a); }
