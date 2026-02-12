105fbae38f42550697807c6a5b17a82d8e0c7768
Nikolay123d <urciknikolaj642@gmail.com>
2026-02-12 01:02:21 +0100

    Delete assets directory
    Signed-off-by: Nikolay123d <urciknikolaj642@gmail.com>

--- assets/js/modules/core/02_state.js
+++
@@ -1,9 +0,0 @@
-
-const KEY = "mk_state";
-
-export function getState(){
-  try{ return JSON.parse(localStorage.getItem(KEY) || "{}"); }catch{ return {}; }
-}
-export function setState(next){
-  localStorage.setItem(KEY, JSON.stringify(next||{}));
-}
